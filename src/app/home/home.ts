0fc744efda70a1153170fc050fe8b03fc09e50d8
kcshamil <shamilshifu6@gmail.com>
2026-01-20 00:08:07 +0530

    recipe page

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,14 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { Header } from '../header/header';
 import { Footer } from '../footer/footer';
+import { RouterLink } from "@angular/router";
+import { ApiService } from '../services/api-service';
 
 
 @Component({
   selector: 'app-home',
-  imports: [Header,Footer],
+  imports: [Header, Footer, RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home {
+  allRecipes:any = signal([])
+  api = inject(ApiService)
+
+  ngOnInit(){
+    this.getAllRecipes()
+  }
+
+  getAllRecipes(){
+    this.api.getAllRecipeAPI().subscribe((res:any)=>{
+      // console.log(res);
+      this.allRecipes.set(res.slice(0,6))
+      console.log(this.allRecipes())
+    })
+  }
 
 }
--- src/app/recipes/recipes.ts
+++ src/app/recipes/recipes.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Footer } from '../footer/footer';
+import { Header } from '../header/header';
 
 @Component({
   selector: 'app-recipes',
-  imports: [],
+  imports: [Header,Footer],
   templateUrl: './recipes.html',
   styleUrl: './recipes.css',
 })
